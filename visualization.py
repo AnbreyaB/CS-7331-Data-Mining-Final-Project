2b16a80efc37e19a1da608869a21a082d9f74edb
Madison H <madharris3350@gmail.com>
2026-04-29 02:35:59 -0500

    Save all visuals to outputs folder

--- visualization.py
+++ visualization.py
@@ -1,3 +1,9 @@
-from scipy.cluster.hierarchy import dendrogram
-from tabulate import tabulate
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+ 
+# Save all charts to outputs folder
+base_dir = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_DIR = os.path.join(base_dir, 'outputs')
+os.makedirs(OUTPUT_DIR, exist_ok=True)
